ccd729075f42c872fddf62b4a6ad20d56194846a
Debarjun Pal <debarjunpal134@gmail.com>
2025-03-31 13:20:34 +0530

    2nd change

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template, request
+import pickle
+import numpy as np # type: ignore
+
+model = pickle.load(open('iri.pkl', 'rb'))
 
 app = Flask(__name__)
 
-@app.route("/",methods=['GET'])
+
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
 def home():
-    return "<h1>First Flask App</h1>"
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    data4 = request.form['d']
+    arr = np.array([[data1, data2, data3, data4]])
+    pred = model.predict(arr)
+    return render_template('after.html', data=pred)
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
